c9038188adcbd86340fe833c388d309215058997
Zijin Cui <zijincui281@berkeley.edu>
2018-11-01 22:16:29 -0700

    Initial Commit

---
+++ Logic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Logic
+//
+//  Created by Zackory Cramer on 11/1/18.
+//  Copyright © 2018 Zackori Cui. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
