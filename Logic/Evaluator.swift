ebc5dce7fdb250795b7ae60d293f8e2a00bd7fc7
Zijin Cui <zijincui281@berkeley.edu>
2018-11-03 15:14:22 -0700

    commit to pull

---
+++ Logic/Evaluator.swift
@@ -0,0 +1,13 @@
+//
+//  Evaluator.swift
+//  Logic
+//
+//  Created by Zackory Cramer on 11/3/18.
+//  Copyright © 2018 Zackori Cui. All rights reserved.
+//
+
+import Foundation
+
+class Evaluator {
+	
+}
